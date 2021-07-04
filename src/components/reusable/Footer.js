3360db7b511abe5cf2b6a3d5eccc0d537eb738be
Avi148 <avi.aggarwal@hippoinnovations.com>
2021-07-04 17:05:27 +0530

    logo

--- src/components/reusable/Footer.js
+++ src/components/reusable/Footer.js
@@ -1,47 +1,91 @@
 import React from 'react'
+import logo from "../../images/logo.jpg"
+import { Link } from 'gatsby';
 
 export default function Footer() {
-    return (
-        <footer>
-          <div className="footer-block">
-          <div className="container-fluid">
+  return (
+
+    <footer>
+
+      <div className="footer-block">
+        <div className="container-fluid">
           <div className="row">
-             <div className="col-12 pt-2 mx-auto text-center bg-dark text-light">
-             <a href="https://twitter.com/intent/tweet/
-               ?text=Check this out
-              &url=https://mariohernandez.io
-                &via=imariohernandez"
-              target="_blank" ><span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="icon icon-twitter"
-          >
-            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-          </svg>
-        </span></a>
-             <h6>Ashish Goyal</h6>
-             <h6>9911550227</h6>
-             <h6>I-555,Alpha-2,Greater Noida,U.P.,India</h6>
-             <h6>KBP &copy; 2021</h6>
-             </div>
-             
+
+            <div className="col-12 pt-2 mx-auto text-center bg-dark text-light">
+
+              <Link to="/" className="navbar-brand ml-3">
+                <img src={logo} alt="logo goes here" width="80px" />
+              </Link>
+              <div>
+                <a href="https://facebook.com/krishnaboxesandpackaging"
+                  target="_blank" ><span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      viewBox="0 0 24 24"
+                      fill-rule="evenodd"
+
+
+                      class="icon icon-facebook"
+                    >
+                      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                    </svg>
+                  </span></a>
+                
+                <a href="https://instagram.com/krishna_boxes_packaging?utm_medium=copy_link"
+                  target="_blank" ><span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      viewBox="0 0 448 512"
+
+
+
+
+                    >
+                      <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                    </svg>
+                  </span></a>
+                
+                <a href="https://api.whatsapp.com/send?phone=+919911550227&text=Hello%2C+I+want+to+buy+Packaging+Boxes"
+                  target="_blank" ><span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="50"
+                      height="50"
+                      viewBox="0 0 32 32"
+                      fill-rule="evenodd"
+
+                      class="whatsapp-ico"
+                    >
+                      <path d="M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z" />
+                    </svg>
+                  </span></a>
+
+              </div>
+
+
+
+              <h6>Ashish Goyal</h6>
+              <h6>9911550227</h6>
+              <h6>krishnaboxesandpackaging@gmail.com</h6>
+              <h6>I-555,Alpha-2,Greater Noida,U.P.,India</h6>
+              <h5 >KBP</h5>
+
+            </div>
+
           </div>
-          </div>
-            
-          </div>
+        </div>
+
+      </div>
 
 
 
 
 
-        </footer>
-    )
+    </footer>
+  )
 }
+
